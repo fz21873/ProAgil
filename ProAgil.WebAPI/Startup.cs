6b6563751e035b78e0709b0ce8c150850cff3a34
fz21873 <fz21873@gmail.com>
2020-12-01 01:44:29 -0400

    respaldo

--- ProAgil.WebAPI/Startup.cs
+++ ProAgil.WebAPI/Startup.cs
@@ -55,7 +55,7 @@ namespace ProAgil.WebAPI
             });*/
     
             services.AddDbContext <ProAgilContext>(
-                            x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+                            x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             
             
             
