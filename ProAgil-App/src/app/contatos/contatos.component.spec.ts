8f6b554d76a0a568b6749abf2d7b527b71873ec5
fz21873 <fz21873@gmail.com>
2020-12-01 02:21:32 -0400

    migrando version de angular 10 para 11

--- ProAgil-App/src/app/contatos/contatos.component.spec.ts
+++ ProAgil-App/src/app/contatos/contatos.component.spec.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
@@ -9,7 +9,7 @@ describe('ContatosComponent', () => {
   let component: ContatosComponent;
   let fixture: ComponentFixture<ContatosComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ContatosComponent ]
     })
